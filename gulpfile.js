131fc9e9bb801f9289bf7a187633a86cb2798f62
Ash <ash@ashryanbeats.com>
2017-04-01 21:35:33 -0400

    Initial commit.

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const livereload = require('gulp-livereload');
+
+gulp.task("reload", () => livereload.reload());
+
+gulp.task("default", () => {
+	livereload.listen();
+
+	gulp.watch(["*", "!./asset/css/**/*"], ["reload"]);
+
+	gulp.watch(["assets/css/sass/*"], ["buildCSS", "reload"]);
+});
+
+gulp.task("buildCSS", () => {
+	return gulp.src("./assets/css/sass/main.scss")
+		.pipe(sass())
+		.pipe(rename("style.css"))
+		.pipe(gulp.dest("./assets/css/"));
+});
--- readme.md
+++ readme.md
@@ -1,5 +1,3 @@
-# ghost-theme-excerpt-by-language
+# Re-Hash theme for Ghost
 
-Change the length of the post excerpt based on language tag.
-
-This repo is a small proof-of-concept. [See this blog post for an explanation](https://blog.ashryan.io/ghost-theme-excerpt-by-language). 
+`gulp` for LiveReload and Sass builds.
